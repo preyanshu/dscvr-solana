4ba434e968e23865c101a1ef99608a817a30bfa8
Sushsnat <grtsushant@gmail.com>
2024-09-06 00:24:55 +0530

    test38

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
               {MintData ? (
                 <MintedTokens MintData={MintData} />
               ) : (
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center w-full h-full items-center'>
                   <div className='spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full' role='status'></div>
                   <span className='ml-2 text-lg'>Loading...</span>
                 </div>
