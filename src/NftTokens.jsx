851be61b9c2cf24d76e36765f08458f41377efc3
Sushsnat <grtsushant@gmail.com>
2024-09-05 12:59:49 +0530

    test13

--- src/NftTokens.jsx
+++ src/NftTokens.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import nfts from './nfts';
 import { GraphQLClient, gql } from 'graphql-request';
 import useCanvasWallet from "./CanvasWalletProvider";
+import {
+    PublicKey,
+    clusterApiUrl,
+    Connection,
+    SystemProgram,
+    Transaction,
+    Keypair
+} from "@solana/web3.js";
 
 const client = new GraphQLClient('https://api.dscvr.one/graphql');
 
@@ -21,152 +29,182 @@ const GET_USER_DATA = gql`
   }
 `;
 
+
 export const NFTDisplay = ({ mintData }) => {
-  const { walletAddress, userInfo } = useCanvasWallet();
-  const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const { walletAddress, userInfo } = useCanvasWallet();
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const fetchUserData = async (username) => {
-    try {
-      setLoading(true);
-      const data = await client.request(GET_USER_DATA, { username });
-      setUserData(data.userByName);
-      setLoading(false);
-    } catch (err) {
-      setError('Failed to fetch user data');
-      setLoading(false);
-    }
-  };
+    const fetchUserData = async (username) => {
+        try {
+            setLoading(true);
+            const data = await client.request(GET_USER_DATA, { username });
+            setUserData(data.userByName);
+            setLoading(false);
+        } catch (err) {
+            setError('Failed to fetch user data');
+            setLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    if (userInfo) {
-      fetchUserData(userInfo.username);
-    }
-  }, [userInfo]);
+    useEffect(() => {
+        if (userInfo) {
+            fetchUserData(userInfo.username);
+        }
+    }, [userInfo]);
 
-  const handleMint = (nftName) => {
-    // Handle mint logic here
-    console.log(`Minting NFT: ${nftName}`);
-  };
+    const handleMint = async (nftName, username) => {
 
-  return (
-    <div className="flex flex-wrap text-xs justify-around p-4">
-      {nfts.map((nft, index) => {
-        const achievement = mintData?.achievements[index];
-        const isAlreadyMinted = achievement?.wallets.some(wallet => wallet?.walletAddress === walletAddress);
+        const asset = Keypair.generate(); // This generates a new keypair
+        const assetPublicKey = asset.publicKey;
+        // Handle mint logic here
+        console.log(`Minting NFT: ${nftName, username}`);
 
-        let mintCondition = null;
-
-        if (index === 0) {
-          // First NFT: Check if dscvrPoints >= 1,000,000
-          if (userData?.dscvrPoints >= 1000000000 && !isAlreadyMinted) {
-            mintCondition = (
-              <button 
-                className="text-sm w-full text-indigo-400"
-                onClick={() => handleMint(nft.name)}
-              >
-                Mint
-              </button>
-            );
-          } else if (isAlreadyMinted) {
-            mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
-          } else {
-            mintCondition = <button className="text-sm w-full text-red">Locked</button>;
-          }
-        } else if (index === 1) {
-          // Second NFT: Check if followerCount >= 1
-          if (userData?.followerCount >= 1 && !isAlreadyMinted) {
-            mintCondition = (
-              <button 
-                className="text-sm w-full text-indigo-400"
-                onClick={() => handleMint(nft.name)}
-              >
-                Mint
-              </button>
-            );
-          } else if (isAlreadyMinted) {
-            mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
-          } else {
-            mintCondition = <button className="text-sm w-full text-red">Locked</button>;
-          }
-        } else if (index === 2) {
-          // Third NFT: Check if streak.dayCount >= 3
-          if (userData?.streak?.dayCount >= 3 && !isAlreadyMinted) {
-            mintCondition = (
-              <button 
-                className="text-sm w-full text-indigo-400"
-                onClick={() => handleMint(nft.name)}
-              >
-                Mint
-              </button>
-            );
-          } else if (isAlreadyMinted) {
-            mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
-          } else {
-            mintCondition = <button className="text-sm w-full hover:border-red-600 text-red-500">Locked</button>;
-          }
-        } else {
-          // Other NFTs: Default mint logic
-          mintCondition = isAlreadyMinted ? (
-            <p className="text-sm text-green-500 font-semibold">Already Minted</p>
-          ) : (
-            <button 
-              className="text-sm w-full text-indigo-400"
-              onClick={() => handleMint(nft.name)}
-            >
-              Mint
-            </button>
-          );
+        try {
+            const tx = await program.methods
+                .createAsset(
+                    "dscvr_points_1000000000",
+                    userData.followerCount,
+                    userData.dscvrPoints,
+                    userData.streak?.dayCount,
+                    username,
+                )
+                .accounts({
+                    signer: new PublicKey(walletAddress),
+                    payer: new PublicKey(walletAddress),
+                    collection: new PublicKey('EEA4LnDi8eXWbrGLXVEBWuUxYG98nZ2dqbrr1FMHLE9o'),
+                    asset: assetPublicKey,
+                    database: new PublicKey('8oPtWBtTKohRGqUDwC2f5JFUgUH5mqBy1vAPzBFFGhzH'),
+                    mplCoreProgram: MPL_CORE_PROGRAM_ID,
+                    systemProgram: anchor.web3.SystemProgram.programId,
+                })
+                .signers([wallet.payer, asset])
+                .rpc();
+            console.log(tx);
+        } catch (error) {
+            console.log(error);
         }
 
-        return (
-          <div 
-            key={index} 
-            className="shadow-lg border border-slate-700 rounded-lg overflow-hidden max-w-[200px] w-full"
-          >
-            <img 
-              src={nft.image} 
-              alt={nft.name} 
-              className="w-full h-[100px] object-cover"
-            />
-            <div className="p-2">
-              <h6 className="text-sm font-bold mb-2">{nft.name}</h6>
-              <div className="relative">
-                <p 
-                  className="text-gray-400 overflow-hidden text-ellipsis" 
-                  style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
-                >
-                  {nft.description}
-                </p>
-                <div 
-                  className="absolute top-[-50px] w-[220px] left-[-20px] opacity-0 hover:opacity-100 transition-opacity duration-300 bg-gray-900 p-4 border rounded-lg shadow-lg z-10"
-                >
-                  <p className="text-gray-200">
-                    {nft.description}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="pb-2 px-2">
-              <div className="text-sm font-semibold text">
-                Minted till now: {achievement.currentCount}/{achievement.maxNftCap}
-              </div>
-              <div className="w-full bg-gray-600 rounded-lg h-1.5 dark:bg-gray-700 my-4">
-                <div
-                  className="bg-indigo-600 h-1.5 rounded-lg"
-                  style={{ width: `${(achievement.currentCount / achievement.maxNftCap) * 100}%` }}
-                ></div>
-              </div>
-              <div className="text-center">
-                {mintCondition}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+    };
+
+    return (
+        <div className="flex flex-wrap text-xs justify-around p-4">
+            {nfts.map((nft, index) => {
+                const achievement = mintData?.achievements[index];
+                const isAlreadyMinted = achievement?.wallets.some(wallet => wallet?.walletAddress === walletAddress);
+
+                let mintCondition = null;
+
+                if (index === 2) {
+                    // First NFT: Check if dscvrPoints >= 1,000,000
+                    if (userData?.dscvrPoints >= 1000000000 && !isAlreadyMinted) {
+                        mintCondition = (
+                            <button
+                                className="text-sm w-full text-indigo-400"
+                                onClick={() => handleMint(codeName, userInfo.username)}
+                            >
+                                Mint
+                            </button>
+                        );
+                    } else if (isAlreadyMinted) {
+                        mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
+                    } else {
+                        mintCondition = <button className="text-sm w-full text-red">Locked</button>;
+                    }
+                } else if (index === 0) {
+                    // Second NFT: Check if followerCount >= 1
+                    if (userData?.followerCount >= 1 && !isAlreadyMinted) {
+                        mintCondition = (
+                            <button
+                                className="text-sm w-full text-indigo-400"
+                                onClick={() => handleMint(nft.codeName, userInfo.username)}
+                            >
+                                Mint
+                            </button>
+                        );
+                    } else if (isAlreadyMinted) {
+                        mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
+                    } else {
+                        mintCondition = <button className="text-sm w-full text-red">Locked</button>;
+                    }
+                } else if (index === 1) {
+                    // Third NFT: Check if streak.dayCount >= 3
+                    if (userData?.streak?.dayCount >= 3 && !isAlreadyMinted) {
+                        mintCondition = (
+                            <button
+                                className="text-sm w-full text-indigo-400"
+                                onClick={() => handleMint(nft.codeName, userInfo.username)}
+                            >
+                                Mint
+                            </button>
+                        );
+                    } else if (isAlreadyMinted) {
+                        mintCondition = <p className="text-sm text-green-500 font-semibold">Already Minted</p>;
+                    } else {
+                        mintCondition = <button className="text-sm w-full hover:border-red-600 text-red-500">Locked</button>;
+                    }
+                } else {
+                    // Other NFTs: Default mint logic
+                    mintCondition = isAlreadyMinted ? (
+                        <p className="text-sm text-green-500 font-semibold">Already Minted</p>
+                    ) : (
+                        <button
+                            className="text-sm w-full text-indigo-400"
+                            onClick={() => handleMint(nft.codeName, userInfo.username)}
+                        >
+                            Mint
+                        </button>
+                    );
+                }
+
+                return (
+                    <div
+                        key={index}
+                        className="shadow-lg border border-slate-700 rounded-lg overflow-hidden max-w-[200px] w-full"
+                    >
+                        <img
+                            src={nft.image}
+                            alt={nft.name}
+                            className="w-full h-[100px] object-cover"
+                        />
+                        <div className="p-2">
+                            <h6 className="text-sm font-bold mb-2">{nft.name}</h6>
+                            <div className="relative">
+                                <p
+                                    className="text-gray-400 overflow-hidden text-ellipsis"
+                                    style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
+                                >
+                                    {nft.description}
+                                </p>
+                                <div
+                                    className="absolute top-[-50px] w-[220px] left-[-20px] opacity-0 hover:opacity-100 transition-opacity duration-300 bg-gray-900 p-4 border rounded-lg shadow-lg z-10"
+                                >
+                                    <p className="text-gray-200">
+                                        {nft.description}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pb-2 px-2">
+                            <div className="text-sm font-semibold text">
+                                Minted till now: {achievement.currentCount}/{achievement.maxNftCap}
+                            </div>
+                            <div className="w-full bg-gray-600 rounded-lg h-1.5 dark:bg-gray-700 my-4">
+                                <div
+                                    className="bg-indigo-600 h-1.5 rounded-lg"
+                                    style={{ width: `${(achievement.currentCount / achievement.maxNftCap) * 100}%` }}
+                                ></div>
+                            </div>
+                            <div className="text-center">
+                                {mintCondition}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default NFTDisplay;
--- src/nfts.jsx
+++ src/nfts.jsx
@@ -1,22 +1,7 @@
 const nfts = [
     {
-      name: "Discovery Dynamo",
-      symbol: "ACHV",
-      description: "You've unlocked the Discovery Dynamo badge by hitting a whopping 1 billion DSCR points! This token celebrates your monumental achievement and relentless pursuit of points.",
-      image: "https://res.cloudinary.com/dbo7hzofg/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1725465197/dscvrpoints_achievments_twtrxq.jpg",
-      attributes: [
-        {
-          trait_type: "Achievement",
-          value: "DSCR Points: 1,000,000,000"
-        },
-        {
-          trait_type: "Token",
-          value: "ACHV"
-        }
-      ]
-    },
-    {
       name: "Follower Frenzy",
+      codeName : "follower_count_1",
       symbol: "ACHV",
       description: "Awarded to those who achieved their first follower milestone in the Follower Frenzy! This token symbolizes your initiation into the world of social influence.",
       image: "https://res.cloudinary.com/dbo7hzofg/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1725465181/follower_achievment_xcmpep.jpg",
@@ -33,6 +18,7 @@ const nfts = [
     },
     {
       name: "Streak Seeker",
+      codeName : "streak_3",
       symbol: "ACHV",
       description: "Congratulations on hitting a 3-day streak on the app! This Streak Seeker token is your badge of consistency and dedication.",
       image: "https://res.cloudinary.com/dbo7hzofg/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1725465190/days_achievment_sglknm.jpg",
@@ -46,7 +32,24 @@ const nfts = [
           value: "ACHV"
         }
       ]
-    }
+    },
+    {
+        name: "Discovery Dynamo",
+        codeName : "dscvr_points_1000000000",
+        symbol: "ACHV",
+        description: "You've unlocked the Discovery Dynamo badge by hitting a whopping 1 billion DSCR points! This token celebrates your monumental achievement and relentless pursuit of points.",
+        image: "https://res.cloudinary.com/dbo7hzofg/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1725465197/dscvrpoints_achievments_twtrxq.jpg",
+        attributes: [
+          {
+            trait_type: "Achievement",
+            value: "DSCR Points: 1,000,000,000"
+          },
+          {
+            trait_type: "Token",
+            value: "ACHV"
+          }
+        ]
+      },
   ];
 
   export default nfts
