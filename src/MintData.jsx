41d2bb30a7e0c09f94240f953a043e5b604c5a00
Sushsnat <grtsushant@gmail.com>
2024-09-05 06:10:43 +0530

    test04

--- src/MintData.jsx
+++ src/MintData.jsx
@@ -1,50 +1,63 @@
 import React, { useEffect } from "react";
 import idl from "./idl.json";
 import {
-  PublicKey,
-  clusterApiUrl,
-  Connection,
-  SystemProgram,
-  Transaction,
+    PublicKey,
+    clusterApiUrl,
+    Connection,
+    SystemProgram,
+    Transaction,
 } from "@solana/web3.js";
 import {
-  AnchorProvider,
-  BN,
-  Program,
+    AnchorProvider,
+    Program,
 } from "@coral-xyz/anchor";
 import { Buffer } from "buffer";
 import useCanvasWallet from "./CanvasWalletProvider";
 
-export const MintData1 = ({setMintData}) => {
+// This is to fix an issue where Buffer might not be available globally
+window.Buffer = Buffer;
+
+export const MintData1 = ({ setMintData }) => {
     const { walletAddress, signTransaction } = useCanvasWallet();
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-    async function init(){
+    async function init() {
+        try {
+            if (!walletAddress) {
+                console.error("Wallet address is not available");
+                return;
+            }
 
-        // console.log("Initializing AnchorProvider", walletAddress);
+            const provider = new AnchorProvider(
+                connection,
+                {
+                    publicKey: new PublicKey(walletAddress),
+                    signTransaction: async (tx) => await signTransaction(tx),
+                },
+                {
+                    commitment: "confirmed",
+                }
+            );
 
-        const provider = new AnchorProvider(connection, {
-            publicKey: new PublicKey(walletAddress),
-            signTransaction,
-        }, {
-            commitment: "confirmed",
-        });
+            const programId = new PublicKey(idl.metadata.address);
+            const program = new Program(idl, programId, provider);
 
-        const program = new Program(idl, provider);
-        // console.log("fsvdfvdfvdfvd",program);
+            const res = await program.account.database.fetch(
+                new PublicKey('8oPtWBtTKohRGqUDwC2f5JFUgUH5mqBy1vAPzBFFGhzH')
+            );
+            console.log("Fetched data:", res);
 
-        const res = await program?.account?.database.fetch(new PublicKey('8oPtWBtTKohRGqUDwC2f5JFUgUH5mqBy1vAPzBFFGhzH'))
-        console.log("a",res);
-
-        setMintData(res);
-
+            setMintData(res);
+        } catch (error) {
+            console.error("Error initializing program:", error);
+        }
     }
-useEffect(() =>{
-    init()
-}, [walletAddress])
-    
-        return (
-            <div>
-            </div>
-        )
-    }
+
+    useEffect(() => {
+        if (walletAddress) {
+            init();
+        }
+    }, [walletAddress]);
+
+    return <div></div>;
+};
