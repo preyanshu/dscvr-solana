3a196453f38bbf200e9b2ce046a145b3792df38a
Sushsnat <grtsushant@gmail.com>
2024-09-06 21:57:54 +0530

    test41

--- src/NftTokens.jsx
+++ src/NftTokens.jsx
@@ -61,6 +61,71 @@ export const NFTDisplay = ({ mintData }) => {
             fetchUserData(userInfo.username);
         }
     }, [userInfo]);
+    // const handleMint = async () => {
+    //     try {
+    //         // Generate a new keypair for the asset
+    //         const asset = Keypair.generate();
+    //         const assetPublicKey = asset.publicKey;
+    
+    //         console.log("Generated Asset Public Key:", assetPublicKey.toBase58());
+    
+    //         // Create a connection to Solana Devnet
+    //         // const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    
+    //         // Create an AnchorProvider
+    //         const provider = new AnchorProvider(connection, wallet, {
+    //             commitment: "confirmed",
+    //           });
+    
+    //         // console.log("Provider created with wallet:", walletAddress);
+    
+    //         // Initialize the program with IDL and provider
+    //         const program = new Program(idl, provider);
+    //         console.log("Program initialized");
+    //         console.log(program)
+    
+    //         // Log the data to be passed into the createAsset method
+    //         // console.log("Minting NFT with the following data:");
+    //         // console.log("NFT Name:", nftName);
+    //         // console.log("Username:", username);
+    //         // console.log("Follower Count (BN):", new BN(userData.followerCount).toString());
+    //         // console.log("DSCVR Points (BN):", new BN(userData.dscvrPoints).toString());
+    //         // console.log("Streak Day Count (BN):", new BN(userData.streak?.dayCount).toString());
+    
+    //         // Prepare account details
+    //         const accounts = {
+    //             signer: provider.wallet.publicKey,
+    //             payer: provider.wallet.publicKey,
+    //             // collection: new PublicKey('BFicfuae445azP2knrhotFi223fiuuE1cTQ12KtyvkLa'),
+    //             asset: assetPublicKey,
+    //             database: new PublicKey('5ahNFeoYAS4HayZWK6osa6ZiocNojNJcfzgUJASicRbf'),
+    //             // mplCoreProgram: MPL_CORE_PROGRAM_ID,
+    //             // systemProgram: SystemProgram.programId,
+    //         };
+    
+    //         console.log("Accounts info:", accounts);
+    
+    //         // Mint the NFT by calling the program's createAsset method
+    //         const tx = await program.methods
+    //             .createAsset(
+    //                 "follower_count_1",           // Some string identifier
+    //                 new BN(50),       // Convert userData to BN (BigNumber)
+    //                 new BN(70),         // Convert DSCVR points to BN
+    //                 new BN(80),    // Convert streak day count to BN
+    //                 "lol"                              // Username string
+    //             )
+    //             .accounts(accounts)
+    //             .signers([asset])
+    //             .rpc();
+    
+    //         console.log("Transaction successful, tx hash:", tx);
+    
+    //     } catch (error) {
+    //         // Handle any errors that occur during the transaction
+    //         // console.log("transaction", tx)
+    //         console.error("Error during minting process:", error);
+    //     }
+    // };
 
     const handleMint = async (nftName, username) => {
         try {
@@ -103,11 +168,10 @@ export const NFTDisplay = ({ mintData }) => {
             const accounts = {
                 signer: new PublicKey(walletAddress),
                 payer: new PublicKey(walletAddress),
-                collection: new PublicKey('EEA4LnDi8eXWbrGLXVEBWuUxYG98nZ2dqbrr1FMHLE9o'),
                 asset: assetPublicKey,
-                database: new PublicKey('8oPtWBtTKohRGqUDwC2f5JFUgUH5mqBy1vAPzBFFGhzH'),
-                mplCoreProgram: MPL_CORE_PROGRAM_ID,
-                systemProgram: SystemProgram.programId,
+                database: new PublicKey('5ahNFeoYAS4HayZWK6osa6ZiocNojNJcfzgUJASicRbf'),
+                // mplCoreProgram: MPL_CORE_PROGRAM_ID,
+                // systemProgram: SystemProgram.programId,
             };
 
             console.log("Accounts info:", accounts);
@@ -116,7 +180,7 @@ export const NFTDisplay = ({ mintData }) => {
             const tx = await program.methods
                 .createAsset(
                     nftName,           // Some string identifier
-                    new BN(50),       // Convert userData to BN (BigNumber)
+                    new BN(userData.followerCount),       // Convert userData to BN (BigNumber)
                     new BN(userData.dscvrPoints),         // Convert DSCVR points to BN
                     new BN(userData.streak?.dayCount),    // Convert streak day count to BN
                     username                              // Username string
