fc73dd03b01e2b4a1e2c6022f49c5a471a834909
Preyanshu <mishrapreyanshu@gmail.com>
2024-09-08 16:17:34 +0530

    ccgvhuhio
    Signed-off-by: Preyanshu <mishrapreyanshu@gmail.com>

--- src/NftTokens.jsx
+++ src/NftTokens.jsx
@@ -113,8 +113,9 @@ export const NFTDisplay = ({ mintData }) => {
       console.log("Transaction", transaction);
     } catch (error) {
         toast.info(
-            "There may be some issue with networks, please try reloading or try after some time", error
-          );
+            `There may be some issue with networks, please try reloading or try again after some time. 
+             If the error persists, try removing the variable 'vwoSn' from local storage and retry. Error: ${error}`
+          );          
       console.error("Error during minting process:", error);
     }
   };
