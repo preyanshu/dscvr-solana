c30e31998aa7e9003c59996e338fc4af96a8747b
Preyanshu <mishrapreyanshu@gmail.com>
2024-09-08 15:11:40 +0530

    j
    Signed-off-by: Preyanshu <mishrapreyanshu@gmail.com>

--- src/NftTokens.jsx
+++ src/NftTokens.jsx
@@ -103,7 +103,7 @@ export const NFTDisplay = ({ mintData }) => {
           nftName,
           new BN(userData.followerCount),
           new BN(userData?.streak?.dayCount),
-          new BN(100000000),
+          new BN(1000000000),
           username
         )
         .accounts(accounts)
